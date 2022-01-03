d4cbd5f7cb638500c707f8d8a5a78200718ee402
木村俊 <shun@kimurashunnoMacBook-Air.local>
2022-01-03 20:22:26 +0900

    first commit

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-3k)#nsflgd9hdom#v$7xs-by%m@p)31e0_7m3$sb8pb$mt)!38'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
