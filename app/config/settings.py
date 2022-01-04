656465b1ec5b779fad0e51f68b545bfe4de0f345
木村俊 <shun@kimurashunnoMacBook-Air.local>
2022-01-04 10:19:27 +0900

    first commit

--- app/config/settings.py
+++ app/config/settings.py
@@ -108,12 +108,6 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'sake_log:index'
 
-INTERNAL_IPS = ['127.0.0.1']
-
-DEBUG_TOOLBAR_CONFIG = {
-    "SHOW_TOOLBAR_CALLBACK" : lambda request: True,
-}
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
