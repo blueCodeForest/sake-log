8a03a4c8a6566585e3554665c44a516085f079b8
木村俊 <shun@kimurashunnoMacBook-Air.local>
2022-01-04 13:49:50 +0900

    first commit

--- sake_log/views.py
+++ sake_log/views.py
@@ -12,15 +12,15 @@ from .models import DisplayAlcoholList, DrankAlcoholList, StatusList
 from .forms import StatusForm, DrinkForm
 
 # 500エラー原因究明の一時的な処理
-from django.views.decorators.csrf import requires_csrf_token
-from django.http import HttpResponseServerError
+# from django.views.decorators.csrf import requires_csrf_token
+# from django.http import HttpResponseServerError
 
-@requires_csrf_token
-def my_customized_server_error(request, template_name='500.html'):
-    import sys
-    from django.views import debug
-    error_html = debug.technical_500_response(request, *sys.exc_info()).content
-    return HttpResponseServerError(error_html)
+# @requires_csrf_token
+# def my_customized_server_error(request, template_name='500.html'):
+#     import sys
+#     from django.views import debug
+#     error_html = debug.technical_500_response(request, *sys.exc_info()).content
+#     return HttpResponseServerError(error_html)
 # ここまで
 
 
