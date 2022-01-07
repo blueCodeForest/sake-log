7dac694de79d74f74a79017f6665a398e3d02c20
木村俊 <shun@kimurashunnoMacBook-Air.local>
2022-01-07 11:47:33 +0900

    Added a reset function at 6 a.m.

--- sake_log/date.py
+++ sake_log/date.py
@@ -4,13 +4,14 @@ from dateutil.relativedelta import relativedelta
 # 指定日時前のAM6時(基準時間)を取得
 def get_standard_dt(days=0, months=0):
     # monthsヶ月、days日前の現在時刻を取得
-    standard_dt = datetime.now() - relativedelta(months=months) - timedelta(days=days)
+    dt = datetime.now() - relativedelta(months=months) - timedelta(days=days)
 
     # 直前のAM6時(基準時間)を取得
-    if standard_dt.time() < time(hour=6):
-        standard_dt -= timedelta(days=1)
+    if dt.time() < time(hour=6):
+        dt -= timedelta(days=1)
 
-    standard_dt.replace(hour=6,minute=0,second=0,microsecond=0)
+    standard_dt = dt.replace(hour=6,minute=0,second=0,microsecond=0)
+    print(f'standard_dt : {standard_dt}')
 
     return standard_dt
 
