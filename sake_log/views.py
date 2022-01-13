a32ff32ce6563d5ccc6cb21bdb4222dbbbb26d5a
木村俊 <shun@kimurashunnoMacBook-Air.local>
2022-01-13 17:15:44 +0900

    Fix filtering range

--- sake_log/views.py
+++ sake_log/views.py
@@ -11,15 +11,15 @@ from .calc import *
 from .graph import *
 
 # 500エラー原因究明の一時的な処理
-from django.views.decorators.csrf import requires_csrf_token
-from django.http import HttpResponseServerError
+# from django.views.decorators.csrf import requires_csrf_token
+# from django.http import HttpResponseServerError
 
-@requires_csrf_token
-def my_customized_server_error(request, template_name='500.html'):
-    import sys
-    from django.views import debug
-    error_html = debug.technical_500_response(request, *sys.exc_info()).content
-    return HttpResponseServerError(error_html)
+# @requires_csrf_token
+# def my_customized_server_error(request, template_name='500.html'):
+#     import sys
+#     from django.views import debug
+#     error_html = debug.technical_500_response(request, *sys.exc_info()).content
+#     return HttpResponseServerError(error_html)
 # ここまで
 
 # 一覧表示させるビュー
@@ -78,7 +78,7 @@ class AlcoholLogView(LoginRequiredMixin, ListView):
     def get_queryset(self, **kwargs):
         queryset = super().get_queryset(**kwargs)
         term = self.kwargs['term']
-        queryset = queryset.filter(created_at__gte=get_start_day(term)).order_by('-created_at')
+        queryset = queryset.filter(user=self.request.user, created_at__gte=get_start_day(term)).order_by('-created_at')
 
         return queryset
 
